3cd8065cf62b8f3452c0a75c2520cb1b6fa7d49e
Bhuvanteja <53339628+bhuvanteja-reddy@users.noreply.github.com>
2019-12-31 12:05:26 +0530

    instance start and stop code writen and tested.

--- Instance_Start_Stop_AWS.py
+++ Instance_Start_Stop_AWS.py
@@ -1,1 +1,38 @@
-print("hello world")
+import sys
+import boto3
+from botocore.exceptions import ClientError
+
+instance_id = "i-02f2ce0986cf70122"
+action = "ON"
+
+ec2 = boto3.client('ec2', region_name='ap-south-1')
+
+
+if action == 'ON':
+    # Do a dryrun first to verify permissions
+    try:
+        ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+
+    # Dry run succeeded, run start_instances without dryrun
+    try:
+        response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
+else:
+    # Do a dryrun first to verify permissions
+    try:
+        ec2.stop_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+
+    # Dry run succeeded, call stop_instances without dryrun
+    try:
+        response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
